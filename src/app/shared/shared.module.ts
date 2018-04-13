402a49712fdcc4b4746cc7e483993135ae3a257d
Michał Garapich <michal@garapich.pl>
2018-04-14 00:16:41 +0200

    shared: add error-alert component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ErrorAlertComponent } from './error-alert/error-alert.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule,
   ],
   declarations: [ErrorAlertComponent],
   exports: [ErrorAlertComponent]
