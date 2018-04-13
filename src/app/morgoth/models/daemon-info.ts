d7661fcb6514e0204e9dbd390c4e6044a4fd8fcc
Michał Garapich <michal@garapich.pl>
2018-04-14 00:33:23 +0200

    MorgothService: add daemon-info

---
+++ src/app/morgoth/models/daemon-info.ts
@@ -0,0 +1,3 @@
+export interface DaemonInfo {
+  version: string;
+}
--- src/app/morgoth/morgoth.service.ts
+++ src/app/morgoth/morgoth.service.ts
@@ -2,34 +2,44 @@ import { Injectable, InjectionToken, Inject } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ServerInfo } from './models/server-info';
 import { Observable } from 'rxjs/Observable';
+import { DaemonInfo } from './models/daemon-info';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { catchError } from 'rxjs/operators';
 
 export const MORGOTH_DOMAIN = new InjectionToken<string>('morgoth.domain');
 
 @Injectable()
 export class MorgothService {
 
+  private daemonEndpoint;
   private serversEndpoint;
 
   constructor(
     private http: HttpClient,
     @Inject(MORGOTH_DOMAIN) private morgothUrl: string
   ) {
+    this.daemonEndpoint = morgothUrl + '/';
     this.serversEndpoint = morgothUrl + '/servers';
   }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof Error) {
+      return new ErrorObservable(error.error.message);
+    } else {
+      return new ErrorObservable(`${error.status}: ${error.statusText}`);
+    }
+  }
+
+  getInfo(): Observable<DaemonInfo> {
+    return this.http.get<DaemonInfo>(this.morgothUrl).pipe(
+      catchError(this.handleError),
+    );
+  }
+
   getServers(): Observable<ServerInfo[]> {
-    return Observable.create((observer) => {
-      this.http.get<ServerInfo[]>(this.serversEndpoint).subscribe(
-        servers => { observer.next(servers); },
-        (error: HttpErrorResponse) => {
-          if (error.error instanceof Error) {
-            observer.error(error.error.message);
-          } else {
-            observer.error(`${error.status}: ${error.statusText}`);
-          }
-        }
-      );
-    });
+    return this.http.get<ServerInfo[]>(this.serversEndpoint).pipe(
+      catchError(this.handleError),
+    );
   }
 
 }
