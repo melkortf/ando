6893439ec2075bdb2651c8ecc1612023994e3ad5
Michał Garapich <michal@garapich.pl>
2018-04-13 23:20:17 +0200

    shared module initial

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ErrorAlertComponent } from './error-alert/error-alert.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [ErrorAlertComponent],
+  exports: [ErrorAlertComponent]
+})
+export class SharedModule { }
