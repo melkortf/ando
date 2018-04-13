788704e4a9880885ce0805aa7119b57f62eb3057
Michał <michal@garapich.pl>
2018-04-13 18:13:42 +0200

    MorgothService: add test

--- src/app/morgoth/morgoth.service.spec.ts
+++ src/app/morgoth/morgoth.service.spec.ts
@@ -1,15 +1,33 @@
 import { TestBed, inject } from '@angular/core/testing';
-
-import { MorgothService } from './morgoth.service';
+import { MorgothService, MORGOTH_DOMAIN } from './morgoth.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('MorgothService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MorgothService]
+      providers: [
+        MorgothService,
+        {
+          provide: MORGOTH_DOMAIN,
+          useValue: 'FAKE_HOST',
+        }
+      ],
+      imports: [
+        HttpClientTestingModule
+      ]
     });
   });
 
-  it('should be created', inject([MorgothService], (service: MorgothService) => {
+  it('should be created', inject([MorgothService, HttpTestingController], (service: MorgothService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should query morgoth api',
+    inject([MorgothService, HttpTestingController], (service: MorgothService, ctrl: HttpTestingController) => {
+
+      service.getServers().subscribe();
+      ctrl.expectOne('FAKE_HOST/servers');
+      ctrl.verify();
+
   }));
 });
