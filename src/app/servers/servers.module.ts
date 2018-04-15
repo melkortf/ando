9fdf7ec10f83891f51c32e83beee5b5dc0ff555e
Michał <michal@garapich.pl>
2018-04-15 16:27:03 +0200

    Add servers module

---
+++ src/app/servers/servers.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ServersRoutingModule } from './servers-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ServersRoutingModule
+  ],
+  declarations: []
+})
+export class ServersModule { }
