c44b464df21ee11ebbc265f6697accec662e8e80
Michał Garapich <michal@garapich.pl>
2018-04-15 22:08:27 +0200

    MorgothService: handle server upates

--- src/app/morgoth/morgoth.service.ts
+++ src/app/morgoth/morgoth.service.ts
@@ -6,6 +6,8 @@ import { DaemonInfo } from './models/daemon-info';
 import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 import { catchError } from 'rxjs/operators';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
+import * as socketIo from 'socket.io-client';
+import { ServerUpdate } from './models/server-update';
 
 export const MORGOTH_DOMAIN = new InjectionToken<string>('morgoth.domain');
 
@@ -13,19 +15,39 @@ export const MORGOTH_DOMAIN = new InjectionToken<string>('morgoth.domain');
 export class MorgothService {
 
   info: ReplaySubject<DaemonInfo>;
-  serversEndpoint: string;
+  servers: ReplaySubject<ServerInfo[]>;
+  ioSocket;
 
   constructor(
     private http: HttpClient,
     @Inject(MORGOTH_DOMAIN) private morgothUrl: string
   ) {
     this.info = new ReplaySubject<DaemonInfo>(1);
+    this.servers = new ReplaySubject<ServerInfo[]>(1);
 
-    this.serversEndpoint = morgothUrl + '/servers';
+    this.ioSocket = socketIo(`${this.morgothUrl}/`);
 
-    this.http.get<DaemonInfo>(this.morgothUrl + '/').subscribe(
+    this.http.get<DaemonInfo>(`${this.morgothUrl}/`).subscribe(
       info => this.info.next(info),
     );
+
+    this.http.get<ServerInfo[]>(`${this.morgothUrl}/servers`).subscribe(
+      servers => {
+        this.servers.next(servers);
+        this.ioSocket.on('serverUpdate', (data: ServerUpdate) => this.updateServer(servers, data));
+      }
+    );
+  }
+
+  private updateServer(servers: ServerInfo[], update: ServerUpdate) {
+    const index = servers.findIndex(s => s.name === update.name);
+    if (index !== -1) {
+      const s = servers[index];
+      servers.splice(index, 1, {
+        ...s,
+        ...update
+      });
+    }
   }
 
   private handleError(error: HttpErrorResponse) {
@@ -41,9 +63,7 @@ export class MorgothService {
   }
 
   getServers(): Observable<ServerInfo[]> {
-    return this.http.get<ServerInfo[]>(this.serversEndpoint).pipe(
-      catchError(this.handleError),
-    );
+    return this.servers.asObservable();
   }
 
 }
