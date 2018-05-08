7bbac9893df8b540084d74fc3f6296592e5514a2
Michał <michal@garapich.pl>
2018-05-08 16:14:48 +0200

    steam link update, tests update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,6 +7,7 @@ import { SharedModule } from './shared/shared.module';
 import { MorgothTestingService } from './morgoth/testing';
 import { MorgothService } from './morgoth/morgoth.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -28,15 +29,28 @@ describe('AppComponent', () => {
       ]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it('should render title in a header tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('header>nav>span').textContent).toContain('melkor.tf');
+    expect(compiled.querySelector('header>nav>span.navbar-brand').textContent).toContain('melkor.tf');
+  }));
+
+  it('should render social links in navbar', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    const github = fixture.debugElement.query(By.css('header>nav>span.navbar-text>a:nth-child(1)')).nativeElement as HTMLAnchorElement;
+    expect(github.href).toBe('https://github.com/melkortf');
+
+    const steam = fixture.debugElement.query(By.css('header>nav>span.navbar-text>a:nth-child(2)')).nativeElement as HTMLAnchorElement;
+    expect(steam.href).toBe('https://steamcommunity.com/id/nieduzy/');
   }));
 });
