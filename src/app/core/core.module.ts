96bc708f0558f5c93857b57ae2651aacf950bb63
Michał <michal@garapich.pl>
2018-05-23 16:33:20 +0200

    add CoreModule

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocialLinksComponent } from './social-links/social-links.component';
+import { environment } from '../../environments/environment';
+import { SOCIAL_LINKS } from './app-config.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    SocialLinksComponent
+  ],
+  providers: [
+    {
+      provide: SOCIAL_LINKS,
+      useValue: environment.social_links
+    }
+  ],
+  exports: [
+    SocialLinksComponent,
+  ]
+})
+export class CoreModule { }
