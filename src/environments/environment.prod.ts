ff2deb2b69dd785a2da68491612a1b3804fb7aee
Michał Garapich <michal@garapich.pl>
2018-06-14 02:31:27 +0200

    feat: add env for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: true,
+  anneDomain: 'http://api.melkor.tf',
+  socialLinks: [
+    {
+      icon: 'github',
+      target: 'https://github.com/melkortf'
+    },
+    {
+      icon: 'steam',
+      target: 'https://steamcommunity.com/id/nieduzy/'
+    }
+  ],
+  pages: [
+    {
+      slug: 'faq',
+      source: 'https://raw.githubusercontent.com/melkortf/documents/master/FAQ.md'
+    }
+  ]
 };
