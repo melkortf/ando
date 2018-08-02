be60f295cf3993a7a91fae9d007a9d28ddd278a9
Michał Garapich <michal@garapich.pl>
2018-08-02 02:16:06 +0200

    test: AppComponent test update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { SharedModule } from './shared/shared.module';
-import { RouterTestingModule } from '@angular/router/testing';
-import { By } from '@angular/platform-browser';
-import { DaemonStatusComponent } from './daemon/daemon-status/daemon-status.component';
-import { DaemonService } from './daemon/daemon.service';
-import { DaemonTestingService } from './daemon/testing/daemon-testing.service';
-import { NgProgressModule } from '@ngx-progressbar/core';
-import { SocialLinksComponent } from './core/social-links/social-links.component';
 import { CoreModule } from './core/core.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         CoreModule,
-        SharedModule,
-        NgProgressModule.forRoot(),
         RouterTestingModule,
       ],
-      declarations: [
-        AppComponent,
-        DaemonStatusComponent,
-      ],
-      providers: [
-        {
-          provide: DaemonService,
-          useClass: DaemonTestingService
-        }
-      ]
+      declarations: [ AppComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
   }));
 
