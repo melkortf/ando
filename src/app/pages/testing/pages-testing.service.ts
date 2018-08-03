8b7386f43b588ee29c890f359d844d32ddeed954
Michał Garapich <michal@garapich.pl>
2018-08-03 02:53:07 +0200

    chore: get rid of PagesTestingService

--- src/app/pages/testing/pages-testing.service.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from '../models/page';
-import { Observable, of } from 'rxjs';
-
-export class PagesTestingService {
-  fetchPage(page: Page): Observable<string> {
-    return of('FAKE_BODY');
-  }
-}
