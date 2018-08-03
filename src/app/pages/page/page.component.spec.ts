5678ab633798edb1114cbf96acfce1668e0875bb
Michał Garapich <michal@garapich.pl>
2018-08-03 02:43:47 +0200

    test: PageComponent test update

--- src/app/pages/page/page.component.spec.ts
+++ src/app/pages/page/page.component.spec.ts
@@ -1,13 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { PageComponent } from './page.component';
-import { Page } from '../models/page';
-import { Observable, of } from 'rxjs';
 import { PagesService } from '../pages.service';
-import { PagesTestingService } from '../testing/pages-testing.service';
 import { By } from '@angular/platform-browser';
-import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgress } from '@ngx-progressbar/core';
 import { MarkdownModule } from 'ngx-markdown';
+import { Page } from '../models/page';
+import { of } from 'rxjs';
+
+class PagesServiceStub {
+  fetchPage(_page: Page) { return of('FAKE_BODY'); }
+}
+
+class NgProgressStub {
+  start() { }
+  complete() { }
+}
 
 describe('PageComponent', () => {
   let component: PageComponent;
@@ -17,14 +25,11 @@ describe('PageComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ PageComponent ],
       imports: [
-        NgProgressModule.forRoot(),
         MarkdownModule.forRoot(),
       ],
       providers: [
-        {
-          provide: PagesService,
-          useClass: PagesTestingService
-        }
+        { provide: PagesService, useClass: PagesServiceStub },
+        { provide: NgProgress, useClass: NgProgressStub },
       ]
     })
     .compileComponents();
@@ -40,6 +45,14 @@ describe('PageComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should render the <markdown> tag', () => {
+    component.body = 'FAKE_BODY';
+    fixture.detectChanges();
+
+    const markdown = fixture.debugElement.query(By.css('markdown'));
+    expect(markdown).toBeTruthy();
+  });
+
   it('should show error alert', () => {
     component.error = 'FAKE_ERROR';
     fixture.detectChanges();
@@ -47,5 +60,31 @@ describe('PageComponent', () => {
     const alert = fixture.debugElement.query(By.css('div.alert')).nativeElement as HTMLElement;
     expect(alert.innerText).toEqual('FAKE_ERROR');
     expect(alert.classList.contains('alert-danger')).toBe(true);
+  });
+
+  describe('#set page()', () => {
+    let mockPage: Page;
+
+    beforeEach(() => {
+      mockPage = { slug: 'FAKE_SLUG', source: 'FAKE_SOURCE' };
+    });
+
+    it('should call PagesService.fetchPage()', () => {
+      const spy = spyOn(TestBed.get(PagesService), 'fetchPage').and.callThrough();
+      component.page = mockPage;
+      expect(spy).toHaveBeenCalledWith(mockPage);
+    });
+
+    it('should start & stop the progress ', fakeAsync(() => {
+      const progress = TestBed.get(NgProgress);
+      const startSpy = spyOn(progress, 'start');
+      const completeSpy = spyOn(progress, 'complete');
+
+      component.page = mockPage;
+      expect(startSpy).toHaveBeenCalled();
+
+      tick();
+      expect(completeSpy).toHaveBeenCalled();
+    }));
   });
 });
