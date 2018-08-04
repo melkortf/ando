6d4a01985df21b042c59a968c16d66b7e56c335e
Michał Garapich <michal@garapich.pl>
2018-08-04 12:06:50 +0200

    refactor: ServersService cleanup, use ANNE_DOMAIN token

--- src/app/servers/servers.service.ts
+++ src/app/servers/servers.service.ts
@@ -1,10 +1,10 @@
-import { Injectable, InjectionToken, Inject } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { ReplaySubject, of } from 'rxjs';
 import { Server } from './models';
 import { ServerImpl } from './server-impl';
-import { AnneEndpointsService } from '../anne-endpoints.service';
+import { ANNE_DOMAIN } from '../anne-domain';
 
 @Injectable({
   providedIn: 'root'
@@ -15,21 +15,17 @@ export class ServersService {
 
   constructor(
     private http: HttpClient,
-    private anneEndpoints: AnneEndpointsService
+    @Inject(ANNE_DOMAIN) private domain: string,
   ) {
-    this.fetchServers();
+    this.http.get<Server[]>(`${this.domain}/servers`)
+      .pipe(
+        map(servers => servers.map(s => new ServerImpl(`${this.domain}/servers`, s))),
+        catchError(() => of([]))
+      )
+      .subscribe(this.servers);
   }
 
   getServers() {
     return this.servers.asObservable();
-  }
-
-  private fetchServers() {
-    this.http.get<Server[]>(this.anneEndpoints.servers)
-      .pipe(
-        map(servers => servers.map(s => new ServerImpl(this.anneEndpoints.servers, s))),
-        catchError(err => of([]))
-      )
-      .subscribe(this.servers);
   }
 }
