b8000d2596df35081aed849d322bc3a8245108b4
Michał Garapich <michal@garapich.pl>
2018-08-05 02:34:55 +0200

    test: ErrorAlertComponent test update

--- src/app/shared/error-alert/error-alert.component.spec.ts
+++ src/app/shared/error-alert/error-alert.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ErrorAlertComponent } from './error-alert.component';
+import { By } from '@angular/platform-browser';
 
 describe('ErrorAlertComponent', () => {
   let component: ErrorAlertComponent;
@@ -21,5 +21,23 @@ describe('ErrorAlertComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should apply correct css classes', () => {
+    component.message = 'FAKE_MESSAGE';
+    fixture.detectChanges();
+
+    const div = fixture.debugElement.query(By.css('div')).nativeElement as HTMLDivElement;
+    expect(div).toBeTruthy();
+    expect(div.classList.contains('alert')).toBe(true);
+    expect(div.classList.contains('alert-danger')).toBe(true);
+  });
+
+  it('should render the message itself', () => {
+    component.message = 'FAKE_MESSAGE';
+    fixture.detectChanges();
+
+    const div = fixture.debugElement.query(By.css('div')).nativeElement as HTMLDivElement;
+    expect(div.innerText).toBe('FAKE_MESSAGE');
   });
 });
